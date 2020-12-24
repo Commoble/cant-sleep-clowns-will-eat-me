4495d5c4c2295a9a5ff85312d8b086600bb9c30e
Commoble <josephbettendorff@yahoo.com>
2020-12-24 14:07:47 -0600

    Remove dead code

--- src/main/java/commoble/cantsleepclownswilleatme/CantSleepClownsWillEatMe.java
+++ src/main/java/commoble/cantsleepclownswilleatme/CantSleepClownsWillEatMe.java
@@ -1,10 +1,7 @@
 package commoble.cantsleepclownswilleatme;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(CantSleepClownsWillEatMe.MODID) // tells forge to construct this during modloading
 public class CantSleepClownsWillEatMe
@@ -17,11 +14,6 @@ public class CantSleepClownsWillEatMe
 	public CantSleepClownsWillEatMe() // invoked by forge due to @Mod
 	{
 		INSTANCE = this;
-		
-		// mod bus has modloading init events and registry events
-		IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
-		// forge bus is for server starting events and in-game events
-		IEventBus forgeBus = MinecraftForge.EVENT_BUS;
 		
 		this.serverConfig = ConfigHelper.register(ModConfig.Type.SERVER, ServerConfig::new);
 		
