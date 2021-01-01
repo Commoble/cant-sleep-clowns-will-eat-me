4fbfe9aa7d213c5c06b608d671a0ca8268930a12
Commoble <josephbettendorff@yahoo.com>
2021-01-01 11:47:51 -0600

    Fixed clientside server list screen warning about mod compatibility. Closes #1

--- src/main/java/commoble/cantsleepclownswilleatme/CantSleepClownsWillEatMe.java
+++ src/main/java/commoble/cantsleepclownswilleatme/CantSleepClownsWillEatMe.java
@@ -1,7 +1,12 @@
 package commoble.cantsleepclownswilleatme;
 
+import org.apache.commons.lang3.tuple.Pair;
+
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Mod(CantSleepClownsWillEatMe.MODID) // tells forge to construct this during modloading
 public class CantSleepClownsWillEatMe
@@ -14,6 +19,9 @@ public class CantSleepClownsWillEatMe
 	public CantSleepClownsWillEatMe() // invoked by forge due to @Mod
 	{
 		INSTANCE = this;
+		
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST,
+			() -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a,b) -> true));
 		
 		this.serverConfig = ConfigHelper.register(ModConfig.Type.SERVER, ServerConfig::new);
 		
